457c3214f3ef8ac7d8a3b37e8fd2de8091ad9e99
TertychnyiRoma <roma.tertychnyi.mail@gmail.com>
2024-05-31 17:40:24 +0300

    Add "search" functionality

--- src/components/albums/AlbumsList/index.tsx
+++ src/components/albums/AlbumsList/index.tsx
@@ -4,9 +4,36 @@ import type { ComponentProps, FC } from 'react';
 import { AlbumItem } from '../AlbumItem';
 import { useAlbumsStore } from '@/providers/albums-store-provider';
 import cn from 'classnames';
+import { useSearchParams } from 'next/navigation';
 
 export const AlbumsList: FC<ComponentProps<'ul'>> = ({ className }) => {
+  const searchParams = useSearchParams();
   const albums = useAlbumsStore((state) => state.albums);
+
+  const filteredAlbums = albums.filter((album) => {
+    const query = searchParams.get('query');
+
+    if (query) {
+      const lowerCaseQuery = query.toLowerCase();
+      const lowerCaseTitle = album.title.toLowerCase();
+      const lowerCaseArtistName = album.artist.fullName.toLowerCase();
+
+      return (
+        lowerCaseTitle.includes(lowerCaseQuery) ||
+        lowerCaseArtistName.includes(lowerCaseQuery)
+      );
+    }
+
+    return true;
+  });
+
+  if (!filteredAlbums.length) {
+    return (
+      <div className='my-12 text-center text-2xl font-medium lg:mt-0'>
+        No albums found.
+      </div>
+    );
+  }
 
   return (
     <ul
@@ -17,7 +44,7 @@ export const AlbumsList: FC<ComponentProps<'ul'>> = ({ className }) => {
         className,
       )}
     >
-      {albums.map((album, i) => (
+      {filteredAlbums.map((album, i) => (
         <li key={i}>
           <AlbumItem album={album} />
         </li>
